bf1049ca77697c1cdcfe24eb391d5d0509ca38e1
Scode0 <gandumdiagne.serena@gmail.com>
2025-05-02 13:09:43 +0200

    Python-0-Starting ex00 done

--- Python-0-Starting/ex00/Hello.py
+++ Python-0-Starting/ex00/Hello.py
@@ -5,12 +5,8 @@ ft_dict = {"Hello" : "titi!"}
 
 ft_list[1] = "World!"
 ft_tuple = ("Hello", "France!")
-if "tutu!" in ft_set:
-    ft_set.remove("tutu!")
-    ft_set.add("Paris!")
-ft_set = ft_set
+ft_set = "{'Hello', 'Paris!'}"
 ft_dict["Hello"] = "42Paris"
-
 
 print(ft_list)
 print(ft_tuple)
