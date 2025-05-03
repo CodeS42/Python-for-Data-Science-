0e8ea7d231a9eb9daf467dde99913d8cf7ac5f03
Scode0 <gandumdiagne.serena@gmail.com>
2025-05-03 02:21:33 +0200

    Python-0-Starting ex04 done

--- Python-0-Starting/ex04/whatis.py
+++ Python-0-Starting/ex04/whatis.py
@@ -3,15 +3,14 @@ import sys
 try:
     if len(sys.argv) > 2:
         raise AssertionError("AssertionError: more than one argument is provided")
-    
-    if  % 2 == 0:
-        print("I'm Even.")
-    else :
-        print("I'm Odd.")
+    elif len(sys.argv) == 2:
+        try:
+            number = int(sys.argv[1])
+        except ValueError:
+            raise AssertionError("AssertionError: argument is not an integer")
+        if number % 2 == 0:
+            print("I'm Even.")
+        else :
+            print("I'm Odd.")
 except AssertionError as e:
     print(e)
-
-try:
-    number = int(sys.argv[1])
-except Exception:
-    
