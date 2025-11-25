d45fbc2378b59851f62026bc15acb90972ad7e46
CodeS42 <149813094+CodeS42@users.noreply.github.com>
2025-11-25 16:32:42 +0100

    Python-0-Starting ex08 done

--- Python-0-Starting/ex08/Loading.py
+++ Python-0-Starting/ex08/Loading.py
@@ -1,33 +1,50 @@
-import os
-import time
+from shutil import get_terminal_size
+from time import time
 
 def ft_tqdm(lst: range) -> None:
-    print_block = round(int(lst[-1]) / 24)
-    print_percent = round(int(lst[-1]) / 100)
-    step_percent = print_percent
-    count = 0
-    term_columns = os.get_terminal_size().columns
-    start = time.time()
-    
+    nb_elements = len(lst)
+    terminal_width = get_terminal_size().columns
+    done = 0
+    time_start = time()
+    last_print = 0
+
     for elem in lst:
-        elapsed_time = time.time() - start
-        if elapsed_time > 0:
-            it_per_sec = round(int(elem + 1) / elapsed_time, 2)
-        else:
-            it_per_sec = 0
-        it_per_sec = str(it_per_sec) + "it/s"
-        
-        # nb_characters = len(lst) + + len(it_per_sec)
-        # max_loading_bar = term_columns - 
-        count += 1
-        if count == print_block:
-            print(int(elem) + 1)
-            # f"100%|████████████████████████| 333/333 [00:01<00:00, 171.42it/s]"
-            # f"{percent}%|{loading_bar}| {passed_elements}/{total_elements} [00:01<00:00, 171.42it/s]"
-            # print(f"\r{str(print_percent)}%|", end="", flush=True)
-            # print_percent += step_percent
-            print('\u2588', end="", flush=True)
-            count = 0
-        if count == 333:
-            print(333)
-        yield lst
+        done += 1
+        if elem == 0 or elem == nb_elements - 1 or (time() - last_print) >= 0.1:
+            percent = int((done / nb_elements) * 100)
+            elapsed = time() - time_start
+            it_per_sec = done / elapsed
+            time_left = (nb_elements - done) / it_per_sec
+
+            hours = int(elapsed // 3600)
+            minutes = int((elapsed % 3600) // 60)
+            seconds = int(elapsed % 60)
+            if hours >= 1:
+                str_elapsed = f"{hours:02d}:{minutes:02d}:{seconds:02d}"
+            else:
+                str_elapsed = f"{minutes:02d}:{seconds:02d}"
+
+            hours = int(time_left // 3600)
+            minutes = int((time_left % 3600) // 60)
+            seconds = int(time_left % 60)
+            if hours >= 1:
+                str_time_left = f"{hours:02d}:{minutes:02d}:{seconds:02d}"
+            else:
+                str_time_left = f"{minutes:02d}:{seconds:02d}"
+
+            if percent < 100:
+                str_percent = f"{percent: 3d}%"
+            else:
+                str_percent = f"{percent}%"
+
+            str_it_per_sec = f"{it_per_sec:.2f}it/s"
+            str_done = f"{done}/{nb_elements}"
+
+            size_loading_bar = terminal_width - len(str_percent) - len(str_elapsed) - len(str_it_per_sec) - len(str_done) - len(str_time_left) - 9
+            nb_blocks = int((size_loading_bar * done) / nb_elements)
+            
+            loading_bar = '█' * nb_blocks + ' ' * (size_loading_bar - nb_blocks)
+            line = f"{str_percent}|{loading_bar}| {str_done} [{str_elapsed}<{str_time_left}, {str_it_per_sec}]"
+            print('\r' + line, end='')
+            last_print = time()
+        yield elem
